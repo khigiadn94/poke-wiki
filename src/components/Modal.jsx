ee5bda7f9de1e560f1460d5f5bbb7e343a4b835c
unknown <nguyenkhanhdn94@gmail.com>
2025-04-27 22:30:52 -0400

    Fix Close Modal function

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ export default function Modal(props) {
   const { children, handleCloseModal } = props;
   return ReactDom.createPortal(
     <div className="modal-container">
-      <button onClick={handleCloseModal} className="modal-overlay" />
+      <div className="modal-underlay" onClick={handleCloseModal} />
       <div className="modal-content">{children}</div>
     </div>,
     document.getElementById('portal'),
